0cd7b5ccca544302673a74399d2aaba70435fb2e
Michael <greenmr95@hotmail.com>
2023-01-26 00:58:51 +0000

    add dashboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,17 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+  
   def show
   end
+
+  def total_products
+    Product.count
+  end
+  helper_method :total_products
+
+  def total_categories
+    Category.count
+  end
+  helper_method :total_categories
+
 end
